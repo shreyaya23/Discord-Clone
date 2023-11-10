18d0261ad859fc83f1fe29cbf223ad437ef430cf
Shreya Singh <119122095+shreyaya23@users.noreply.github.com>
2023-11-10 18:36:32 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: { 
+      fontFamily:{
+      Whitney:["Whitney"],
+      Ginto:["Ginto"],
+      ggSans:["ggSans"],
+    },},
+  },
+  plugins: [],
+ 
+}
+
